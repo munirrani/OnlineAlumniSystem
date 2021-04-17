9e8dac06b7725aebe96d5dc0ee6887b62b4df777
sirhafizho <hafizemail135@gmail.com>
2021-04-18 03:45:01 +0800

    just temporarily adds onclick to the login to go to profile for debugging

--- main.js
+++ main.js
@@ -6,36 +6,47 @@ $(function () {
 });
 
 
-let checkPassEmpty = function () {
-    if (document.getElementById('password').value == "") {
-        document.getElementById('error-text-reg').style.display = "none";
-        return true;
-    }
-}
+
 // firstname
 // lastname
 
-let check = function () {
+let checkpw = function () {
 
-
+    if (document.getElementById('password').value == "" && document.getElementById('confirmpw').value == "") {
+        document.getElementById('confirmpw').classList.remove("input-error");
+        document.getElementById('error-text-reg').classList.remove("error-reg-text");
+    }
     if (document.getElementById('confirmpw').value != "") {
         if (document.getElementById('password').value != document.getElementById('confirmpw').value) {
             document.getElementById('error-text-reg').classList.add("error-reg-text");
             document.getElementById('confirmpw').classList.add("input-error");
             document.getElementById('error-text-reg').innerHTML = 'Confimed Password Does Not Match';
             return false;
-        } else {
+        } else{
             document.getElementById('confirmpw').classList.remove("input-error");
             document.getElementById('error-text-reg').classList.remove("error-reg-text");
-            return true;
         }
     } else {
         document.getElementById('confirmpw').classList.remove("input-error");
         document.getElementById('error-text-reg').classList.remove("error-reg-text");
     }
-
+    if(document.getElementById('password').value == document.getElementById('confirmpw').value) {
+        return true;
+    }
 
 }
+
+
+
+// document.querySelector(".submitreg").onsubmit = function () {
+//     if(check()){
+//         window.location.href = "profile.html";
+//     }
+// };
+
+
+
+
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
