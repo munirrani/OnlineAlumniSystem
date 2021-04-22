e485107dce862b208a5690b4ba585ec73299bc5d
Humaira' AS <you@example.com>
2021-04-22 13:26:50 +0800

    edit job-details

--- js/main.js
+++ js/main.js
@@ -86,23 +86,16 @@ window.onclick = function (event) {
   }
 };
 
-var link = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
-
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } 
+    });
 }
-
-
-
-
-
-
