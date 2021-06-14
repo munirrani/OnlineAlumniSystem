48dbfaa68f88dc52227e3c2653a7d0b33e1ad32d
Fahad Thakur <ft.thakur1@gmail.com>
2021-06-14 07:13:16 +0300

    Made changes to login and register pg head section

--- register.php
+++ register.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<head>  
+    <?php include_once("php/head.php") ?>
+    <?PHP
+
+    if (isset($_SESSION["userid"])) {
+        header("location: index.php");
+    }
+
+    ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-    <script src="https://kit.fontawesome.com/d4305da033.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="icon" href="img/logo_um_without_text.png" type="image/png">
-    <script type="text/javascript" src="js/map.js"></script>
-    <script src="https://kit.fontawesome.com/d4305da033.js" crossorigin="anonymous"></script>
     <title>Register | FSKTM Alumni</title>
 </head>
 
