a4e2bc1b0b6d073c0ae921d0f4893359c4e6096c
Muhammad Hafiz <hafizemail135@gmail.com>
2021-06-17 19:36:40 +0800

    change to heroku connection db

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "herokuonline";
+  $servername = "eu-cdbr-west-01.cleardb.com";
+  $username = "b4154d122e1a7a";
+  $password = "4f7ae1f2";
+  $dbname = "heroku_25bcb2cb4d9b1e8";
   $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
   if ($conn->ping()) {
